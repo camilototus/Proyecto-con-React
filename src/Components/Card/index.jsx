53bf6ac5acb7f4fb28ad623f1c853296c64e80af
camilo rios <camilototus180@gmail.com>
2023-06-22 17:26:46 -0500

    en el componente OrderCart se creo la visualizacion de los productos tambien anulando el boton de agregar con un Check

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 
@@ -19,6 +19,28 @@ const Card = (data) => {
         console.log('contex ',context.cartProducts)
     }
 
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+
+        if (isInCart) {
+            return (
+                <div 
+                        className="absolute top-0 right-0 flex justify-center items-center bg-black/50 w-6 h-6 rounded-full m-2 p-1">
+                        <CheckIcon className="h-6 w-6 text-white" />
+                </div>
+            )
+        } else {
+            return (
+                <div 
+                        className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+                        onClick={(event) => addProductsToCart(event, data.data)}>
+                        <PlusIcon className="h-6 w-6 text-black" />
+                </div>
+            )
+        }
+
+    }
+
     return (
         <div 
             className="bg-white cursor-pointer w-56 h-60 rounded-lg"
@@ -26,11 +48,7 @@ const Card = (data) => {
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.tittle} />
-                <div 
-                    className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={(event) => addProductsToCart(event, data.data)}>
-                    <PlusIcon className="h-6 w-6 text-black-500" />
-                </div>
+                {renderIcon(data.data.id)}
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.data.title}</span>
--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCart from '../OrderCart'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
+    console.log('cart: ', context.cartProducts)
 
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -15,6 +17,18 @@ const CheckoutSideMenu = () => {
                     className="cursor-pointer h-6 w-6 text-black-500" 
                     onClick={context.closeCheckoutSideMenu}/>
                 </div>
+            </div>
+            <div className='px-6 overflow-y-scroll'>                
+            {
+                context.cartProducts.map(product => (
+                    <OrderCart 
+                    key={product.id}
+                    title={product.title}
+                    imageUrl={product.images}
+                    price={product.price}
+                    />
+                ))
+            }
             </div>
         </aside>
     )
