e75bfc653d661219262747f44c8076162e1ecfd1
camilo rios <camilototus180@gmail.com>
2023-06-28 17:19:23 -0500

    se configuro la Key de account y sign out en localStorage

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -6,6 +6,13 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     const context = useContext(ShoppingCartContext)
     const activeStyle = "underline underline-offset-4"
+
+    const handleSignOut = () => {
+        const stringifiedSignOut = JSON.stringify(true)
+        localStorage.setItem('sign-out', stringifiedSignOut)
+        context.setSignOut(true)
+    }
+
     return (
         <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
             <ul className="flex items-center gap-3">
@@ -103,8 +110,9 @@ const Navbar = () => {
                     <NavLink to= '/sign-in'
                     className={({ isActive }) =>
                     isActive ? activeStyle : undefined
-                    }>
-                        Sign In
+                    }
+                    onClick={() => handleSignOut()}>
+                        Sign out
                     </NavLink>
                 </li> 
                <li className='flex items-center'>
