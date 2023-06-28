03ef83b1e685b1e929d196ce89ce1bdaddaf5ff4
camilo rios <camilototus180@gmail.com>
2023-06-28 18:16:41 -0500

    si hizo una funcion en el navbar para que se vieran o no las paginas si se cliqueaba SignOut y se organizo y estilizo Sign In con User y Password

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,10 +7,73 @@ const Navbar = () => {
     const context = useContext(ShoppingCartContext)
     const activeStyle = "underline underline-offset-4"
 
+    // SIGN OUT
+    const signOut = localStorage.getItem('sign-out')
+    const parsedSignOut = JSON.parse(signOut)
+    const isUserSignOut = context.signOut || parsedSignOut
+
     const handleSignOut = () => {
         const stringifiedSignOut = JSON.stringify(true)
         localStorage.setItem('sign-out', stringifiedSignOut)
         context.setSignOut(true)
+    }
+    
+    const renderView = () => {
+        if (isUserSignOut) {
+            return (
+                <li>
+                <NavLink to= '/sign-in'
+                className={({ isActive }) =>
+                isActive ? activeStyle : undefined
+                }
+                onClick={() => handleSignOut()}>
+                    Sign out
+                </NavLink>
+            </li> 
+            )
+        } else {
+            return (
+                <>
+                    <li className="text-black/60">
+                    camilototus180@gmail.com
+                </li> 
+               <li>
+                    <NavLink to= '/my-order'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        My Order
+                    </NavLink>
+                </li> 
+               <li>
+                    <NavLink to= '/my-orders' 
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        My Orders
+                    </NavLink>
+                </li> 
+               <li>
+                    <NavLink to= '/my-account'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        My Account
+                    </NavLink>
+                </li> 
+               <li>
+                    <NavLink to= '/sign-in'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                    }
+                    onClick={() => handleSignOut()}>
+                        Sign out
+                    </NavLink>
+                </li> 
+               
+                </>
+            )
+        }
     }
 
     return (
@@ -79,42 +142,7 @@ const Navbar = () => {
                 </li> 
             </ul>
             <ul className="flex items-center gap-3">
-               <li className="text-black/60">
-                    camilototus180@gmail.com
-                </li> 
-               <li>
-                    <NavLink to= '/my-order'
-                    className={({ isActive }) =>
-                    isActive ? activeStyle : undefined
-                    }>
-                        My Order
-                    </NavLink>
-                </li> 
-               <li>
-                    <NavLink to= '/my-orders' 
-                    className={({ isActive }) =>
-                    isActive ? activeStyle : undefined
-                    }>
-                        My Orders
-                    </NavLink>
-                </li> 
-               <li>
-                    <NavLink to= '/my-account'
-                    className={({ isActive }) =>
-                    isActive ? activeStyle : undefined
-                    }>
-                        My Account
-                    </NavLink>
-                </li> 
-               <li>
-                    <NavLink to= '/sign-in'
-                    className={({ isActive }) =>
-                    isActive ? activeStyle : undefined
-                    }
-                    onClick={() => handleSignOut()}>
-                        Sign out
-                    </NavLink>
-                </li> 
+               {renderView()} 
                <li className='flex items-center'>
                <ShoppingCartIcon className="h-6 w-6 text-black-500" />
                <div>
--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,34 @@
+import { Link } from "react-router-dom"
 import Layout from "../../Components/Layout"
 
 function SignIn() {
     return (
       <Layout>
-        SignIn
+        <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+        <div className="flex flex-col w-80">
+          <p>
+            <span className="font-ligth text-sm">Email: </span>
+            <span>camilototus180@gmail.com</span>
+          </p>
+          <p>
+            <span className="font-ligth text-sm">Password: </span>
+            <span>********</span>
+          </p>
+          <Link
+            to="/">
+              <button
+                className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+                Log in
+              </button>
+          </Link>
+          <div>
+            <a className="font-ligth text-xs underline underline-offset-4" href="/">Forgot my Password</a>
+          </div>
+          <button
+            className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3">
+            Sign Up
+          </button>
+        </div>
       </Layout>
     )
   }
