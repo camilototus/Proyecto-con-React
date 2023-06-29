fab78bf6f09e295300ac473312c840f2917e45ef
camilo rios <camilototus180@gmail.com>
2023-06-29 17:45:45 -0500

    se agrego la info de la creacion del usuaria a local storage y al estado global y hacer sign In(sign-out en false)

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState, useRef } from "react"
 import { ShoppingCartContext } from "../../Context"
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 import Layout from "../../Components/Layout"
 
 function SignIn() {
@@ -8,13 +8,21 @@ function SignIn() {
   const [view, setView] = useState('user-info')
   const form = useRef(null)
 
-// Account
+    // Account
   const account =localStorage.getItem('account')
   const parsedAccount = JSON.parse(account)
     // Has an account
-  const noAccountInLocalStorage = parsedAccount ? object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+
+  const handleSignIn = () => {
+    const stringifiedSignOut = JSON.stringify(false)
+    localStorage.setItem('sign-out', stringifiedSignOut)
+    context.setSignOut(false)
+    // Redirect
+    return <Navigate replace to={'/'} />
+  }
 
   const createAnAccount = () => {
     const formData = new FormData(form.current)
@@ -23,6 +31,12 @@ function SignIn() {
       email: formData.get('email'),
       password: formData.get('password')
     }
+    //create account
+    const stringifiedAccount = JSON.stringify(data)
+    localStorage.setItem('account', stringifiedAccount)
+    context.setAccount(data)
+    //sign in
+    handleSignIn()
   }
 
   const renderLogin = () => {
@@ -30,7 +44,7 @@ function SignIn() {
       <div className="flex flex-col w-80">
           <p>
             <span className="font-ligth text-sm">Email: </span>
-            <span>{parsedAccount?.gmail}</span>
+            <span>{parsedAccount?.email}</span>
           </p>
           <p>
             <span className="font-ligth text-sm">Password: </span>
@@ -40,6 +54,7 @@ function SignIn() {
             to="/">
               <button
                 className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+                onClick={() => handleSignIn()}
                 disabled={!hasUserAnAccount}>
                 Log in
               </button>
